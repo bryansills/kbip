388dbb8ee10861c704ff04128c08f89c69a4ba75
bryansills <bryan.sills@gatech.edu>
2013-09-07 03:20:49 +0000

    done with code. all i have to do is set up the experiments.

--- agent.py
+++ agent.py
@@ -14,10 +14,109 @@ class Agent:
         return result
 
     def winningSpots(self, board):
-        return []
+        result = []
+
+        # check horizontal x
+        for row in range(3):
+            count = 0
+            for ind in range(3):
+                if board[row][ind].state == self.side:
+                    count = count + 1
+
+            if count >= 2:
+                for ind in range(3):
+                    if board[row][ind].state == '-':
+                        result.append((row, ind))
+
+        # check vertical x
+        for col in range(3):
+            count = 0
+            for ind in range(3):
+                if board[ind][col].state == self.side:
+                    count = count + 1
+
+            if count >= 2:
+                for ind in range(3):
+                    if board[ind][col].state == '-':
+                        result.append((ind, col))
+
+        # check diagonal x
+        count = 0
+        for ind in range(3):
+            if board[ind][ind].state == self.side:
+                count = count + 1
+
+        if count >= 2:
+            for ind in range(3):
+                if board[ind][ind].state == '-':
+                    print('gonna win')
+                    result.append((ind, ind))
+
+
+        count = 0
+        for ind in range(3):
+            if board[2 - ind][ind].state == self.side:
+                count = count + 1
+
+        if count >= 2:
+            for ind in range(3):
+                if board[2 - ind][ind].state == '-':
+                    print('will win')
+                    result.append((2 - ind, ind))
+
+        return result
+
 
     def blockLosingSpots(self, board):
-        return []
+        result = []
+
+        # check horizontal x
+        for row in range(3):
+            count = 0
+            for ind in range(3):
+                if board[row][ind].state != self.side and board[row][ind].state != '-':
+                    count = count + 1
+
+            if count >= 2:
+                for ind in range(3):
+                    if board[row][ind].state == '-':
+                        result.append((row, ind))
+
+        # check vertical x
+        for col in range(3):
+            count = 0
+            for ind in range(3):
+                if board[ind][col].state != self.side and board[ind][col].state != '-':
+                    count = count + 1
+
+            if count >= 2:
+                for ind in range(3):
+                    if board[ind][col].state == '-':
+                        result.append((ind, col))
+
+        # check diagonal x
+        count = 0
+        for ind in range(3):
+            if board[ind][ind].state != self.side and board[ind][ind].state != '-':
+                count = count + 1
+
+        if count >= 2:
+            for ind in range(3):
+                if board[ind][ind].state == '-':
+                    result.append((ind, ind))
+
+
+        count = 0
+        for ind in range(3):
+            if board[2 - ind][ind].state != self.side and board[2 - ind][ind].state != '-':
+                count = count + 1
+
+        if count >= 2:
+            for ind in range(3):
+                if board[2 - ind][ind].state == '-':
+                    result.append((2 - ind, ind))
+
+        return result
 
     def oppositeCornerSpots(self, board):
         result = []
